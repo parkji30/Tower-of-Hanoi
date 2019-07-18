3119be37605c41fadae367face4200beaf4b5735
James Park <jinsoo.park@mail.utoronto.ca>
2019-07-18 15:17:15 -0400

    Update gui_controller.py

--- Tower of Hanoi/gui_controller.py
+++ Tower of Hanoi/gui_controller.py
@@ -1,27 +1,6 @@
 """ GUIController: GUI window for manually solving Anne Hoy's problems.
 """
 
-
-# Copyright 2013, 2014, 2017 Gary Baumgartner, Dustin Wehr,
-# Danny Heap, Bogdan Simion, Jacqueline Smith, Dan Zingaro
-# Distributed under the terms of the GNU General Public License.
-#
-# This file is part of Assignment 1, CSC148, Winter 2017.
-#
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this file.  If not, see <http://www.gnu.org/licenses/>.
-
-
 import time
 import tkinter as tk
 from gui_viewables import CheeseView, StoolView
